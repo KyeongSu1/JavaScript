ef51f5f1ec8352d236ec028fc9d20d250898dc9b
KyeongSu1 <102003145+KyeongSu1@users.noreply.github.com>
2022-09-05 11:34:12 +0900

    Add files via upload
    9월5일 3교시

---
+++ plus.js
@@ -0,0 +1,20 @@
+function plus(num1,num2){
+    let result;  /* num1+num2 = 값의 저장소 만드는것 */
+    result=num1+num2; /* result <- 임의저장소에 저장해라 */
+    return result /*result의 들어있는 값을 돌려줘라(return) */
+}
+function minus(num1,num2){
+    let result;  
+    result=num1-num2; 
+    return result 
+}
+function division(num1,num2){
+    let result;  
+    result=num1/num2; 
+    return result 
+}
+function multiplication(num1,num2){
+    let result;  
+    result=num1*num2; 
+    return result 
+}
